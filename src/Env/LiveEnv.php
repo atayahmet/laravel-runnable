00cc52bf32653127e3a16c0483e4581f254e9d39
Ahmet ATAY <ahmetatay@positive.com.tr>
2016-10-16 13:58:05 +0300

    Usability of variable history

--- src/Env/LiveEnv.php
+++ src/Env/LiveEnv.php
@@ -15,11 +15,12 @@ class LiveEnv extends BaseEnvironment {
 
     protected $name = 'live';
     protected $description = 'Run raw sql';
-    protected $lineText = '';
+    protected $lineText = 0;
     protected $model;
     protected $table;
     protected $listTable = true;
     protected $time = 0;
+    protected $vars = [];
     protected $io;
 
     public function __construct(\App\User $model)
@@ -35,6 +36,8 @@ class LiveEnv extends BaseEnvironment {
         DB::listen(function($sql) {
             $this->time = $sql->time;
         });
+
+        $this->lineText = 0;
     }
 
     /**
@@ -46,16 +49,54 @@ class LiveEnv extends BaseEnvironment {
     public function handle($command)
     {
         try {
+            ++$this->lineText;
 
-            eval("\$result = {$command};");
+            extract($this->vars, EXTR_OVERWRITE);
+
+            preg_match('/^(\$[a-zA-Z0-9\-\>]+)\s*?\=\s*?(.*)\;*$/', $command, $matches);
+
+            if(sizeof($matches) > 0) {
+                eval("{$matches[1]} = {$matches[2]};\$result = {$matches[1]};");
+                preg_match('/\$([a-zA-Z0-9]+)/', $matches[1], $varName);
+                $this->vars[$varName[1]] = current(compact($varName[1]));
+            }
+
+            elseif(preg_match('/^(\$[a-zA-Z0-9\-\>]+)\s*?$/', $command)) {
+                eval("\$result = {$command};");
+            }
+
+            else if(preg_match('/\$([a-zA-Z0-9]+)\s*?/', $command)) {
+
+                ob_start();
+                eval($command);
+                $result = ob_get_clean();
+            }else{
+                eval("\$result = {$command};");
+            }
+
             $this->io->newLine(2);
-            $this->io->text('<yellow>></yellow> <green>' . (is_bool($result) ? ($result ? 'true' : 'false') : $result).'</green>');
+
+            if(isset($result)) {
+
+                if(is_bool($result)) {
+                    $this->io->text('<yellow>></yellow> <green>' . ($result ? 'true' : 'false') . '</green>');
+                }
+                else if(is_object($result) || is_array($result)) {
+                    dump($result);
+                }else{
+                    $this->io->text('<yellow>></yellow> <green>' . $result . '</green>');
+                }
+            }
             return;
 
         }catch(\ErrorException $e) {
-            //dump($command);
+            $this->io->newLine(2);
             $this->io->error($e->getMessage());
         }
+    }
 
+    public function tab($input)
+    {
+        dump($input);
     }
 }
--- src/Env/SqlEnv.php
+++ src/Env/SqlEnv.php
@@ -142,6 +142,4 @@ class SqlEnv extends BaseEnvironment {
         }
         return $newData;
     }
-
-
 }
